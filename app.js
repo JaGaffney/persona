e23bae671b3e281c1907ede349fa5f8f69d5d2b5
Jon Gaffney <jagaffney661@msn.com>
2018-12-28 15:34:08 +1100

    data is now displayed on the website, needs a lot more filters for each type

--- app.js
+++ app.js
@@ -1,156 +1,64 @@
-// https://www.youtube.com/watch?v=JaMCxVWtW58
-// use getElementByClassName > queryselector as it 99% quicker
-//var test = require('test.js');
-//import { square } from './test.js';
 import { info } from './config.js';
 
+class PersonaUI{
 
-console.log(info["persona"]["Chariot"])
-// Book Class: Represents a Book
-class Book {
-    constructor(title, author, isbn){
-        this.title = title
-        this.author = author
-        this.isbn = isbn
-    }
-}
-
-// UI Class: Handle UI Tasks
-class UI {
-    static displayBooks() {
-        const books = Store.getBooks()
-        
-        books.forEach((book) => UI.addBookToList(book))
+    static loadPersona() {
+        for (let item in info["persona"]) {
+            for (let persona in info["persona"][item]){
+                let name = persona
+                let arcana = item
+                let level = info["persona"][item][persona]["data"]["level"]
+                let cost = info["persona"][item][persona]["base_cost"]
+            
+                PersonaUI.createPersona(name, arcana, level, cost)
+            }  
+        }
     }
 
-    static addBookToList(book) {
+    static createPersona(name, arcana, level, cost) {
         // get thee id
-        const list= document.getElementById('book-list')
+        const list = document.getElementById('persona-list')
         // declare what type of html you want
         const row = document.createElement('tr')
 
         row.innerHTML = `
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.isbn}</td>
+            <td>${name}</td>
+            <td>${arcana}</td>
+            <td>${level}</td>
+            <td>¥${cost}</td>
             <td><a href="#" class="btn btn-danager btn-sm delete">X</a></td>
         `
 
         list.appendChild(row)
     }
 
-    static deleteBook(elem) {
-        if (elem.classList.contains('delete')) {
-            elem.parentElement.parentElement.remove()
-            // need to remove parent element x 2 to remove further back can go even further
+    static tableFilter() {
+        // Declare variables 
+        var input, filter, table, tr, td, i
+        input = document.getElementById("search-bar")
+        filter = input.value.toUpperCase()
+        table = document.getElementById("persona-list")
+        tr = table.getElementsByTagName("tr")
+
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0]
+            if (td) {
+                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = ""
+                } else {
+                    tr[i].style.display = "none"
+                }
+            } 
         }
-    }
-
-    static showAlert(message, className){
-        const div = document.createElement('div')
-        div.className = `alert alert-${className}`
-        div.appendChild(document.createTextNode(message))
-
-        // parent element
-        const container = document.querySelector('.container')
-        // where you want the code to go
-        const form = document.getElementById('book-form')
-
-        // so inside the container but before for form
-        container.insertBefore(div, form)
-
-        // Vanish in 3 seconds
-        setTimeout(() => document.querySelector('.alert').remove(), 3000)
-    }
-
-    static clearFields(){
-        document.getElementById('title').value = ''
-        document.getElementById('author').value = ''
-        document.getElementById('isbn').value = ''
-    }
-
-
-}
-
-// Store Class: Handles Storage (locally inside the browser)
-class Store {
-    static getBooks() {
-        let books
-        if (localStorage.getItem('books') === null) {
-            books = []
-        } else {
-            books = JSON.parse(localStorage.getItem('books'))
-        }
-
-        return books
-    }
-
-    static addBook(book) {
-        const books = Store.getBooks()
-        books.push(book)
-
-        localStorage.setItem('books', JSON.stringify(books))
-    }
-
-    static removeBook(isbn) {
-        const books = Store.getBooks()
-
-        books.forEach((book, index) => {
-            if (book.isbn === isbn ) {
-                books.splice(index, 1)
-            }
-        })
-
-        localStorage.setItem('books', JSON.stringify(books))
     }
 }
 
 
-// Events: Display Books
-document.addEventListener('DOMContentLoaded', UI.displayBooks)
+// Event: On website load, loads the JSON file and populates the table
+document.addEventListener('DOMContentLoaded', PersonaUI.loadPersona)
 
-// Events: Add a Books
-document.getElementById('book-form').addEventListener('submit', (e) => {
-    // Prevent actual submit
-    e.preventDefault()
-
-    // Get form values
-    const title = document.getElementById('title').value
-    const author = document.getElementById('author').value
-    const isbn = document.getElementById('isbn').value
-
-    // Data validation
-    if (title === '' || author === '' || isbn === '') {
-        UI.showAlert('Please fill in all fields', 'danger')
-    } else {
-           // Instantiate book
-        const book = new Book(title, author, isbn)
-
-        // Add to the UI
-        UI.addBookToList(book)
-
-        // Add book to store
-        Store.addBook(book)
-
-        // show success message
-        UI.showAlert('Book Added', 'success')
-
-        // Clear Fields
-        UI.clearFields() 
-    }
-
-})
-
-// Events: Remove a Books
-// Need to use event propigation to target the table then remove the item from it, rather than just removing the item from the table
-document.getElementById('book-list').addEventListener('click', (e) => {
-    // remove book from UI
-    UI.deleteBook(e.target)
-
-    // remove book from store
-    // get parenet element, then closets table row then its content
-    Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
-
-    // show success message
-    UI.showAlert('Book Removed', 'success')
+// Events: Search bar
+document.getElementById('search-bar').addEventListener('keyup', (e) => {
+    PersonaUI.tableFilter()
 })
